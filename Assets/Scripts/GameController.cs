69c72b101dc558a3e8f4c6902dd0b37d55624e4c
Fabio Alves <fabioalves101@gmail.com>
2015-08-10 17:29:44 -0400

    Implementar waves de inimigos

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,16 +4,29 @@ using System.Collections;
 public class GameController : MonoBehaviour {
     public GameObject hazard;
     public Vector3 spawnValues;
+    public float hazardCount;
+    public float spawnWait;
+    public float hazardWait;
+    
 
     void Start()
     {
-        SpawnWaves();
+        Cursor.visible = false;
+        StartCoroutine(SpawnWaves());
     }
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(hazardWait);
+            }
+            yield return new WaitForSeconds(spawnWait);
+        }        
     }
 }
